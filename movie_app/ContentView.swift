5e8b2e26863dfd6add6f34edca9fa886c036df7f
Fazliddin <faz.ibragimovv@gmail.com>
2026-04-24 17:12:38 +0500

    Initial Commit

---
+++ movie_app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  movie_app
+//
+//  Created by Fazliddin Ibragimov on 24/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ movie_app/movie_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  movie_appApp.swift
+//  movie_app
+//
+//  Created by Fazliddin Ibragimov on 24/04/26.
+//
+
+import SwiftUI
+
+@main
+struct movie_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
